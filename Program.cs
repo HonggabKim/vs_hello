62174e956d643912f1bcb4f80ca843c24fa50387
hgkim <hgkim@pinesns.co.kr>
2017-04-26 20:56:32 +0900

    fix bug in Program.cs

--- Program.cs
+++ Program.cs
@@ -18,8 +18,9 @@ namespace ConsoleApplication1
             Console.WriteLine("modified on GitHub");
             Console.WriteLine("???");
             Console.Write("-------------------");
-         
-            Console.Write("modified again on GitHub")_
+
+            Console.Write("modified again on GitHub");
+
         }
     }
 }
