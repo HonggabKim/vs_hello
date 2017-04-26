4b2fdd1d9c207ae5c1066d0357199172e30c9545
hgkim <hgkim@pinesns.co.kr>
2017-04-26 20:47:34 +0900

    nothing

--- Program.cs
+++ Program.cs
@@ -14,9 +14,7 @@ namespace ConsoleApplication1
             Console.WriteLine("World is mine");
             Console.WriteLine("Luna is mine");
             Console.WriteLine("Earth is mine");
-
             Console.WriteLine("Modified locally");
-            
             Console.WriteLine("modified on GitHub");
             
         }
